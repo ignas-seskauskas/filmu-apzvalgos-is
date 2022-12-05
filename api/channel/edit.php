d114c334749a232e1570b25ade87a229b454b862
ignas_seskauskas <ignas.6k2ch@gmail.com>
2022-12-06 01:19:12 +0200

    fix

--- api/channel/edit.php
+++ api/channel/edit.php
@@ -3,9 +3,9 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
   $GLOBALS['_channelController']->editChannel((object) $_POST);
   $result = array('success' => 'success');
 
-  //echo json_encode($result);
-  //header("Content-Type: application/json; charset=UTF-8");
-  //header("Access-Control-Allow-Origin: *");  
+  echo json_encode($result);
+  header("Content-Type: application/json; charset=UTF-8");
+  header("Access-Control-Allow-Origin: *");  
   die();
 } else {
   $result = array('error' => 'Bad request method');
